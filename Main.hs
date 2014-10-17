ce10cbcba7080966cab1f7a6940d52b357233196
Ricky Elrod <ricky@elrod.me>
2014-10-17 00:20:13 -0400

    Chart multiple people at once
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- Main.hs
+++ Main.hs
@@ -1,23 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Control.Lens
-import Data.Aeson (Value)
+import Control.Monad (join)
 import Data.Aeson.Lens
+import Data.List (intersperse)
 import Data.Scientific
 import Graphics.Rendering.Chart.Easy
 import Graphics.Rendering.Chart.Backend.Diagrams
 import Network.Wreq
 import System.Environment (getArgs)
 
-getWPMs :: String -> IO [Value]
+getWPMs :: String -> IO [(Int, Double)]
 getWPMs username = do
   wpms <- get $ "https://typeracerdata.appspot.com/games?playerId=tr:" ++ username ++ "&n=2000&offset=0"
-  return $ wpms ^.. responseBody . values
+  return $ wpms ^.. responseBody . values ^@.. reindexed (+1) (reversed . traversed <. key "wpm" . _Number . to toRealFloat)
 
 main :: IO ()
 main = do
-  [username] <- getArgs
-  wpms <- getWPMs username
-  let wpms' = wpms ^@.. reindexed (+1) (reversed . traversed <. key "wpm" . _Number . to toRealFloat)
-  toFile def (username ++ ".svg") $ do
+  usernames <- getArgs
+  plots <- mapM plot' usernames
+  toFile def ((join . intersperse "_" $ usernames) ++ ".svg") $ do
     layout_title .= "Typeracer WPM History"
-    plot (line "WPM" [wpms' :: [(Int, Double)]])
+    mapM_ id plots
+
+plot' :: String -> IO (EC (Layout Int Double) ())
+plot' username = do
+  wpms <- getWPMs username
+  return $ plot (line username [wpms])
